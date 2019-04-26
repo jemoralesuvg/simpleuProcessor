fe775ae3372387c9fed7fb856882d6cfac1d0999
jose laptop <jemorales@uvg.edu.gt>
2019-04-25 18:47:06 -0600

    Program counter y memoria

---
+++ Inst_Memory.v
@@ -0,0 +1,14 @@
+module Inst_Memory(
+  input clk, enable,
+  input[10:0]address,
+  output[13:0] data_out
+);
+  wire [13:0] data_out;
+  reg [13:0] memory [0:1024];
+  
+  assign data_out = memory[address];
+  
+initial begin
+ $readmemb("instructions.list", memory);
+end
+endmodule
